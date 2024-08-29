8fb22a5fc3e1c48ac5be980f50fe0e805fab4b19
Ushio Shinohara <louishinohara@gmail.com>
2024-08-29 15:40:48 -0400

    init front end in react-native with a few screens
    Co-authored-by: Louis Shinohara <louishinohara@gmail.com>

---
+++ frontend/app/(auth)/_layout.jsx
@@ -0,0 +1,38 @@
+import { Redirect, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+import Loader from "../../components/Loader";
+// import { useGlobalContext } from "../../context/GlobalProvider";
+
+const AuthLayout = () => {
+  // const { loading, isLogged } = useGlobalContext();
+
+  const loading = false;
+  const isLogged = false;
+
+  if (!loading && isLogged) return <Redirect href="/home" />;
+
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+
+      <Loader isLoading={loading} />
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
+};
+
+export default AuthLayout;
