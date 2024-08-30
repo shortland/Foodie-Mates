741172a44587413ac6f3fe1b28eb109e5178b7d6
root <ilankleiman@gmail.com>
2024-08-30 03:59:37 +0000

    feat: add way to define args & data fields that are required for an endpoint

--- backend/public/Controllers/Controller.php
+++ backend/public/Controllers/Controller.php
@@ -6,7 +6,45 @@ class Controller {
 
     public function route(string $method, string $endpoint, Array $query_string, Array $bin_data, Array $cookies) {
         if (isset($this->endpoints[$method][$endpoint])) {
-            $result = $this->{$this->endpoints[$method][$endpoint]}($query_string, $bin_data, $cookies);
+            
+            // check that user is logged in if endpoint requires it
+            if (isset($this->endpoints[$method][$endpoint]['auth'])) {
+                if ($this->endpoints[$method][$endpoint]['auth']) {
+                    if (!isset($cookies[SESS_ID])) {
+                        echo (new HttpResponse)->json(new ErrorRes('not logged in'));
+                        return;
+                    }
+
+                    $cookie_path = SESS_COOKIES_PATH . "/sess_" . $cookies[SESS_ID];
+                    if (!file_exists($cookie_path)) {
+                        # current cookie user provided is not valid (anymore?)
+                        echo (new HttpResponse)->json(new ErrorRes('not logged in'));
+                        return;
+                    }
+                }
+            }
+
+            // check for required data
+            if (isset($this->endpoints[$method][$endpoint]['data'])) {
+                foreach ($this->endpoints[$method][$endpoint]['data'] as $data) {
+                    if (!isset($bin_data[$data])) {
+                        echo (new HttpResponse)->json(new ErrorRes('missing data: ' . $data));
+                        return;
+                    }
+                }
+            }
+
+            // check for missing query string data
+            if (isset($this->endpoints[$method][$endpoint]['query'])) {
+                foreach ($this->endpoints[$method][$endpoint]['query'] as $query) {
+                    if (!isset($query_string[$query])) {
+                        echo (new HttpResponse)->json(new ErrorRes('missing query: ' . $query));
+                        return;
+                    }
+                }
+            }
+
+            $result = $this->{$this->endpoints[$method][$endpoint]["function"]}($query_string, $bin_data, $cookies);
             
             echo (new HttpResponse)->json($result);
         } else {
